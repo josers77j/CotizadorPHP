8ae98ff9b23031046479bb1d8adfc3561eef0d7b
josers77j <josers77.1@gmail.com>
2023-01-13 16:15:59 -0600

    APP_NAME in header/navbar

--- templates/includes/navbar.php
+++ templates/includes/navbar.php
@@ -20,7 +20,7 @@
     <div class="navbar navbar-dark bg-dark shadow-sm">
       <div class="container">
         <a href="#" class="navbar-brand d-flex align-items-center">
-          <strong>Cotizador</strong>
+          <strong><?php echo APP_NAME;?></strong>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
